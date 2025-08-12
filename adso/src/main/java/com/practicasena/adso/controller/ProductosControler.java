8039604859cb17292141df455222f06c45603507
leonardo <leonardobautista67@gmail.co>
2025-08-12 17:21:22 -0500

    segundo commit

---
+++ adso/src/main/java/com/practicasena/adso/controller/ProductosControler.java
@@ -0,0 +1,63 @@
+package com.practicasena.adso.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practicasena.adso.models.Productos;
+import com.practicasena.adso.repositoryes.ProductosRepositorio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+
+
+@RestController
+public class ProductosControler {
+    @Autowired
+    private ProductosRepositorio prodRepo;
+
+
+  @GetMapping("/GEISITO")
+  public String llamarProductos() {
+      return prodRepo.findAll().toString();
+  }
+  @GetMapping("/ListaProductos")
+  public List<Productos> llamarprodlist() {
+      return prodRepo.findAll();
+  }
+  
+
+  @PostMapping("/AgregarProductos")
+  public Productos agregarP(@RequestBody Productos productos) {
+      return prodRepo.save(productos);
+  }
+  
+
+  @PutMapping("/ActualisandoProd/{id}")
+  public String ActualizarProd(@PathVariable String id, @RequestBody Productos productos) {
+      Productos actualizarProductos = prodRepo.findById(Integer.parseInt(id)).orElse(null);
+    if (actualizarProductos == null){
+        return "producto no encontrado";
+    }
+    actualizarProductos.setNombre(productos.getNombre());
+    actualizarProductos.setPrecio(productos.getPrecio());
+    actualizarProductos.setCategoria(productos.getCategoria());
+    prodRepo.save(actualizarProductos);
+    return "producto actualizado";
+  }
+
+  @DeleteMapping("/Borrando/{id}")
+  private String eliminarProducto(@PathVariable Integer id){
+    prodRepo.deleteById(id);
+return "eliminao";
+  }
+}
--- adso/src/main/java/com/practicasena/adso/models/Usuario.java
+++ adso/src/main/java/com/practicasena/adso/models/Usuario.java
@@ -1,5 +1,7 @@
 package com.practicasena.adso.models;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,4 +26,6 @@ public class Usuario {
     private String nombre;
     private String apellido;
     private String ciudad;
+
+    
 }
